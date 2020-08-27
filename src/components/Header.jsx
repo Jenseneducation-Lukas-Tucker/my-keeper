2c58a9dbcbfc877d44dc2ed91bc7b55786874568
frotuck <54941820+frotuck@users.noreply.github.com>
2020-08-27 16:12:09 +0200

    done with the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import EcoIcon from '@material-ui/icons/Eco';
 
 function Header() {
     return (
     <header>
-        <h1>Keeper</h1>
+        <h1><EcoIcon style={{color: "green"}}/>Keeper</h1>
     </header>
     );
 }
